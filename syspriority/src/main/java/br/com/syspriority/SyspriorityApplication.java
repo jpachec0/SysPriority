2bf4949e80ce1fa7b131bb7fbec2a47fd0f688f7
jpachec0 <bcreeper825@gmail.com>
2026-02-08 15:04:39 -0300

    Primeiro commit - Primeira tela (não funcional) - 02/08/26

---
+++ syspriority/src/main/java/br/com/syspriority/SyspriorityApplication.java
@@ -0,0 +1,13 @@
+package br.com.syspriority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyspriorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyspriorityApplication.class, args);
+	}
+
+}
---
+++ syspriority/src/test/java/br/com/syspriority/SyspriorityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.syspriority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyspriorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
